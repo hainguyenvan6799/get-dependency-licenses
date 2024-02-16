0aa117a9de97058e0e2e334b858872e30ab949a0
Hai Nguyen <hai.nguyen@kyanon.digital>
2024-02-16 11:02:37 +0700

    get dependency's license

---
+++ index.js
@@ -0,0 +1,54 @@
+const FS = require('fs');
+
+const packageJSON = JSON.parse(FS.readFileSync('package.json').toString());
+const dependencies = packageJSON.dependencies;
+
+const LICENSE_NAMES = [
+  'LICENSE',
+  'license',
+  'LICENSE.md',
+];
+
+let FAILED_TO_FIND_LICENSES = false;
+
+let libraries = Object.keys(dependencies);
+
+libraries.forEach(key => {
+  console.log('\n' + key);
+  console.log('================================');
+
+  const path = 'node_modules/' + key;
+
+  let license = null;
+
+  LICENSE_NAMES.forEach(name => {
+    try {
+      license = FS.readFileSync(path + '/' + name).toString();
+    } catch (e) {}
+  });
+
+  if (!license) {
+    try {
+      let libraryPackageFile = FS.readFileSync(path + '/package.json').toString();
+      // You can try to get the license from the package.json here
+    } catch (e) {}
+  }
+
+  if (license) {
+    console.log(license);
+  } else {
+    console.log('License not found');
+    FAILED_TO_FIND_LICENSES = true;
+
+    try { // This is to help update the LICENSE_NAMES before re-run it
+      let files = FS.readdirSync(path);
+      files.forEach(file => {
+        console.log(file);
+      });
+    } catch (e) {}
+  }
+});
+
+if (FAILED_TO_FIND_LICENSES) {
+  console.error('WARN: The license for some dependencies was not found!');
+}
